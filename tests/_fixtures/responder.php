291c0c7a25f4f5209b7307006fca9c8734a74e83
Moritz Friedrich <moritz.friedrich@messengerpeople.com>
2020-08-27 22:48:52 +0200

    Added status endpoint, clarifying comments

--- tests/_fixtures/responder.php
+++ tests/_fixtures/responder.php
@@ -25,6 +25,7 @@ try {
     $response = $factory->createResponse(Status::OK);
 
     switch ($request->getUri()->getPath()) {
+        // Loopback endpoint: Responds with request information in a JSON body.
         case '/loopback':
             $response = $response->withBody(createJsonBody([
                 'method'  => $request->getMethod(),
@@ -36,10 +37,28 @@ try {
             ]));
             break;
 
+        // Body endpoint: Mirrors the request body in the response body
         case '/body':
             $response = $response->withBody($request->getBody());
             break;
 
+        // Status endpoint: Returns a response with the status code specified in
+        // the "code" query parameter.
+        case '/status':
+            $desiredStatus = (int)($request->getQueryParams()['code'] ?? 200);
+            $message = Status::getMessage($desiredStatus) ?: '';
+
+            fwrite(
+                STDERR,
+                "Responding with {$desiredStatus}: {$message}\n"
+            );
+
+            $response = $response
+                ->withStatus($desiredStatus)
+                ->withBody(Stream::create($message));
+            break;
+
+        // By default, a 404 error will be generated.
         default:
             $error = "No handler for URI: {$request->getUri()->getPath()}";
             $response = $response
